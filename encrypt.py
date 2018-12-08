2de23da7dcac04e0a470ecf203238fd11ccd7e1d
Frajerzycki <32599796+Frajerzycki@users.noreply.github.com>
2018-12-08 18:42:11 +0100

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,25 +1,23 @@
 from random import randint
-from decimal import Decimal
+from fraction import Fraction
 
-def sq(a):
-    return a*a
 
 
 def encrypt(m, p, k):
-    e = Decimal(randint(k << 20, k << 2048))
-    m = Decimal(m)
-    p = Decimal(p)
-    k = Decimal(k)
+    #e = Fraction(randint(k.a, k.a << 10))
+    e = Fraction(20)
     a = (e - k) / (m - p)
-    b = e - m * a
-    r = sq(m) + sq(e)
-    x = (sq(b) - r) / ((sq(a) + 1) * m)
-    y = a * x - b
-    return (round(x), y)
+    b = e - m*a
+    r = m*m + e*e
+    
+    x = (b*b - r) / ((a*a + Fraction(1))*m)
+    y = a*x + b
+    return (x,y)
 
-m = Decimal(input("Podaj wartość m:\t"))
-p = int(input("Podaj wartość p:\t"))
-k = int(input("Podaj wartość k:\t"))
+m = Fraction(int(input("Podaj wartość m:\t")))
+p = Fraction(int(input("Podaj wartość p: ")))
+k = Fraction(int(input("Podaj wartość k: ")))
+
 C = encrypt(m, p, k)
-print("x = " + str(C[0]))
-print("y = " + str(C[1]))
+print("x = {}".format(C[0]))
+print("y = {}".format(C[1]))
